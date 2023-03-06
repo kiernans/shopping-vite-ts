60d48e50034265b1d23f6dc88e9598a56b76a51e
Kiernan Soriano <ksoriano10@horizon.csueastbay.edu>
2023-03-05 20:42:11 -0700

    Create Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Cart from './routes/Cart';
 import Home from './routes/Home';
 import Shop from './routes/Shop';
 
 function App() {
 	return (
-		<div className='App'>
+		<div className='container'>
+			<Navbar />
 			<Routes>
 				<Route path='' element={<Home />}></Route>
 				<Route path='shop' element={<Shop />}></Route>
+				<Route path='cart' element={<Cart />}></Route>
 				<Route path='*' element={<h1>Page Not Found</h1>}></Route>
 			</Routes>
 		</div>
